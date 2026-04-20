6f84d75ab554668a2d3c5b68053a29b63562df10
Peter <s214236@dtu.dk>
2026-04-20 14:18:03 +0200

    fixed scenario_creator to work with any combination of data

--- src/assignment_2/utils/scenario_creator.py
+++ src/assignment_2/utils/scenario_creator.py
@@ -4,28 +4,29 @@ from itertools import product
 
 
 def create_scenarios(
-    input_data: list[dict[str, list[float]]],
+    input_data: dict[str, dict[str, list[float]]],
 ) -> list[dict[str, float | dict[str, list[float]]]]:
     """Create all scenarios for step 1 of assignment 2.
 
     Args:
-        input_data (list[dict[str, list[float]]]): A list of dictionaries,
-            each containing a key and a list of 24 values.
+        input_data (dict[str, dict[str, list[float]]]): A dictionary containing the input data for each scenario.
 
     Returns:
         list[dict[str, float | dict[str, list[float]]]]: A list of all scenarios, and their weights. Set equal for all scenarios.
     """
+    data_keys = list(input_data.keys())
     result: list[dict[str, float | dict[str, list[float]]]] = [
         {
             "weight": 1.0,
             "data": {
-                "wind_forecast": wind,
-                "da_price_forecast": da_price,
-                "system_imbalance_forecast": system_imbalance,
+                data_key: values
+                for data_key, (_, values) in zip(
+                    data_keys, scenario_combination, strict=True
+                )
             },
         }
-        for wind, da_price, system_imbalance in product(
-            *[data.values() for data in input_data]
+        for scenario_combination in product(
+            *(scenario_data.items() for scenario_data in input_data.values())
         )
     ]
 
@@ -39,7 +40,11 @@ if __name__ == "__main__":
         load_wind_power,
     )
 
-    input_data = [load_wind_power(), load_da_prices(), load_system_imbalance()]
+    input_data = {
+        "wind_power": load_wind_power(),
+        "da_prices": load_da_prices(),
+        "system_imbalance": load_system_imbalance(),
+    }
     scenarios = create_scenarios(input_data)
 
     print(f"Created {len(scenarios)} scenarios.")
